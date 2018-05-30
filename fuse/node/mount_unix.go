edab478a145eab1768bbafa2324447734c10ec12
Péter Szilágyi <peterke@gmail.com>
2018-05-30 21:30:23 +0000

    Ungx-ed commit 9bb6174847fb63a95d7a30737cc54891bbf7b959

--- fuse/node/mount_unix.go
+++ fuse/node/mount_unix.go
@@ -1,5 +1,4 @@
-// +build linux darwin freebsd netbsd openbsd
-// +build !nofuse
+// +build !windows,!nofuse
 
 package node
 
@@ -9,11 +8,12 @@ import (
 	"strings"
 	"sync"
 
+	logging "github.com/ipsn/go-ipfs/gxlibs/ipfs/QmRb5jh8z2E8hMGN2tkvs1yHynUanqnZ3UeKwgN1i9P1F8/go-log"
+
 	core "github.com/ipsn/go-ipfs/core"
 	ipns "github.com/ipsn/go-ipfs/fuse/ipns"
 	mount "github.com/ipsn/go-ipfs/fuse/mount"
 	rofs "github.com/ipsn/go-ipfs/fuse/readonly"
-	logging "github.com/ipsn/go-ipfs/gxlibs/ipfs/QmRb5jh8z2E8hMGN2tkvs1yHynUanqnZ3UeKwgN1i9P1F8/go-log"
 )
 
 var log = logging.Logger("node")
