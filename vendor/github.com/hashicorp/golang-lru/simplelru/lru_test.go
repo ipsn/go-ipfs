f4fb254c0a7d23dc5e6981f12e197bdfff5316c6
Péter Szilágyi <peterke@gmail.com>
2019-01-01 23:24:26 +0000

    Ungx-ed commit 5a42289a030d6a804acea0f2ad79ae00c45621dd

--- vendor/github.com/hashicorp/golang-lru/simplelru/lru_test.go
+++ vendor/github.com/hashicorp/golang-lru/simplelru/lru_test.go
@@ -8,7 +8,7 @@ func TestLRU(t *testing.T) {
 		if k != v {
 			t.Fatalf("Evict values not equal (%v!=%v)", k, v)
 		}
-		evictCounter++
+		evictCounter += 1
 	}
 	l, err := NewLRU(128, onEvicted)
 	if err != nil {
@@ -112,7 +112,7 @@ func TestLRU_GetOldest_RemoveOldest(t *testing.T) {
 func TestLRU_Add(t *testing.T) {
 	evictCounter := 0
 	onEvicted := func(k interface{}, v interface{}) {
-		evictCounter++
+		evictCounter += 1
 	}
 
 	l, err := NewLRU(1, onEvicted)
